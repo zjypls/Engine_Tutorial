2f62ad298ee73bcb2465646eac4c2bcf6c98578a
zjypls <61042663+zjypls@users.noreply.github.com>
2023-09-12 23:33:12 +0800

    add a optimize handle to use discrete GPU
    correct script build flag

--- ZEditor/App.cpp
+++ ZEditor/App.cpp
@@ -4,23 +4,29 @@
 #include "./ZEditor.h"
 #include "Z/Core/Entry.h"
 
-//TODO:optimize this with glad
-//extern "C"
-//{
-//	__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
-//}
+//make application use discrete GPU
+extern "C" {
+// http://developer.download.nvidia.com/devzone/devcenter/gamegraphics/files/OptimusRenderingPolicies.pdf
+// The following line is to favor the high performance NVIDIA GPU if there are multiple GPUs
+// Has to be .exe module to be correctly detected.
+__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+
+// And the AMD equivalent
+// Also has to be .exe module to be correctly detected.
+__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 0x00000001;
+}
 
 class Editor : public Z::Application {
 public:
-	Editor(const Z::ApplicationSpec&spec):Application(spec) {
+	Editor(const Z::ApplicationSpec &spec) : Application(spec) {
 		PushLayer(new Z::EditorLayer());
 	}
 
 	~Editor() override = default;
 };
 
-Z::Application *Z::GetApplication(const Z::CommandArgs& args) {
-	return new Editor({ "ZEditor",args,Z_SOURCE_DIR});
+Z::Application *Z::GetApplication(const Z::CommandArgs &args) {
+	return new Editor({"ZEditor", args, Z_SOURCE_DIR});
 }
 
 
