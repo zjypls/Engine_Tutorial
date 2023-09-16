8b6636966b626389dceb1a7ccafbb8709b6b2668
zjypls <61042663+zjypls@users.noreply.github.com>
2023-09-17 00:44:11 +0800

    fix bug sample error texture
    should be finished with a material

--- Shaders/EasySample.glsl
+++ Shaders/EasySample.glsl
@@ -37,7 +37,7 @@ struct Data{
 };
 
 layout(location=0)in Data inData;
-layout(binding=2)uniform sampler2D Base;
+layout(binding=0)uniform sampler2D Base;
 layout(binding=3)uniform UniformData{
     mat4 model;
     int indexID;
