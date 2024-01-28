202162b520af1569575ecb8003a9a77b1bd4a14d
zjypls <zjypls@foxmail.com>
2024-01-28 17:48:18 +0800

    improve CMakeLists

---
+++ Engine/Z/Renderer/RenderPass.cpp
@@ -0,0 +1,8 @@
+//
+// Created by z on 24-1-28.
+//
+
+#include "RenderPass.h"
+
+namespace Z {
+} // Z
---
+++ Engine/Z/Renderer/RenderPass.h
@@ -0,0 +1,21 @@
+//
+// Created by z on 24-1-28.
+//
+
+#ifndef RENDERPASS_H
+#define RENDERPASS_H
+
+
+
+namespace Z {
+
+class RenderPass {
+public:
+    struct FrameBuffer {
+
+    };
+};
+
+} // Z
+
+#endif //RENDERPASS_H
