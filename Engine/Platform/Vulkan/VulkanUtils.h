79662e7aec05c355f37e0c348b9cc09bf4de8fff
zjypls <zjypls@foxmail.com>
2024-02-26 19:35:39 +0800

    fix ui color
    fix the strange color caused by the error swapchain image format on Windows and Linux with wayland

--- Engine/Platform/Vulkan/VulkanUtils.h
+++ Engine/Platform/Vulkan/VulkanUtils.h
@@ -243,7 +243,7 @@ namespace Z{
 
         VkSurfaceFormatKHR chooseSwapChainFormat(const std::vector<VkSurfaceFormatKHR>&formats){
             for(auto format:formats){
-                if(format.format==VK_FORMAT_R8G8B8A8_SRGB&&format.colorSpace==VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+                if(format.format==VK_FORMAT_B8G8R8A8_UNORM&&format.colorSpace==VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
                     return format;
             }
             return formats[0];
